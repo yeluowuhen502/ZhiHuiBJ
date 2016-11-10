b73bbae93cb92acc6f5f955fb79d6d7c0b634752
1036012350@qq.com <JTQc5235>
2016-11-10 14:05:51 +0800

    Initial commit

---
+++ app/src/main/java/winning/zhihuibj/MainActivity.java
@@ -0,0 +1,13 @@
+package winning.zhihuibj;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
